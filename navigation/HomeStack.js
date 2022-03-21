a2ece537f07e0cdac9bc118ddd9607f2519b821a
Olivia <oliviabrz@gmail.com>
2022-03-21 16:17:49 -0500

    commit

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screens/HomeScreen';
-import Profile from '../screens/Profile'
 
 const Stack = createStackNavigator();
 
@@ -10,7 +9,6 @@ export default function HomeStack() {
   return (
     <Stack.Navigator headerMode='none'>
       <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='Profile' component={Profile} />
     </Stack.Navigator>
   );
 }
