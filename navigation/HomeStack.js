821f6c4c078bec1472f72125198c490894bb9388
Lei Shi <leishi567@gmail.com>
2022-03-21 17:11:37 -0400

    added tab navigator in homescreen

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screens/HomeScreen';
-
+import PersonalPage from '../screens/PersonalPage'
 
 const Stack = createStackNavigator();
 
@@ -10,7 +10,7 @@ export default function HomeStack() {
   return (
     <Stack.Navigator headerMode='none'>
       <Stack.Screen name='Home' component={HomeScreen} />
-     
+      <Stack.Screen name='PersonalPage' component={PersonalPage} />
     </Stack.Navigator>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useContext } from 'react';
-import { StyleSheet, Text, View, RNButton } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import { IconButton } from '../components';
-import Firebase from '../config/firebase';
-import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
+import { IconButton, Button } from "../components";
+import Firebase from "../config/firebase";
+import { AuthenticatedUserContext } from "../navigation/AuthenticatedUserProvider";
 
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Tab = createMaterialBottomTabNavigator();
 const auth = Firebase.auth();
+//const Tab = createMaterialBottomTabNavigator();
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   const { user } = useContext(AuthenticatedUserContext);
   const handleSignOut = async () => {
     try {
@@ -19,49 +24,70 @@ export default function HomeScreen() {
   };
   return (
     <View style={styles.container}>
-      <StatusBar style='dark-content' />
+      <StatusBar style="dark-content" />
       <View style={styles.row}>
         <Text style={styles.title}>Welcome {user.email}!</Text>
         <IconButton
-          name='logout'
+          name="logout"
           size={24}
-          color='#fff'
+          color="#fff"
           onPress={handleSignOut}
         />
       </View>
       <Text style={styles.text}>Your UID is: {user.uid} </Text>
-      {/* <RNButton
-        onPress={() => navigation.navigate('PersonalPage')}
-        //title='Go to Personal Page'
-        color='#fff'
-      /> */}
+      <Button
+        onPress={() => navigation.navigate("PersonalPage")}
+        title="Go to Personal Page"
+        color="#fff"
+      />
+      <Tab.Navigator
+        initialRouteName="Feed"
+        labeled={false}
+        tabBarOptions={{
+          showIcon: true,
+          showLabel: false,
+          indicatorStyle: {
+            opacity: 0,
+          },
+        }}
+        barStyle={{ backgroundColor: "#ffffff" }}
+      >
+        <Tab.Screen
+          key={Date.now()}
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </View>
-
-   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e93b81',
+    backgroundColor: "#e93b81",
     paddingTop: 50,
     paddingHorizontal: 12,
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
   },
   title: {
     fontSize: 24,
-    fontWeight: '600',
-    color: '#fff',
+    fontWeight: "600",
+    color: "#fff",
   },
   text: {
     fontSize: 16,
-    fontWeight: 'normal',
-    color: '#fff',
+    fontWeight: "normal",
+    color: "#fff",
   },
 });
