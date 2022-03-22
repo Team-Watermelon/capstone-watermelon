658a8155471c9ca9de861a0e0d9b5b70f7fd825a
Olivia <oliviabrz@gmail.com>
2022-03-21 19:51:55 -0500

    got nav bar up and running

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import HomeScreen from '../screens/HomeScreen';
+import Messages from '../screens/Messages';
 
 const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
-export default function HomeStack() {
+export default function HomeTabs() {
   return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      initialRouteName='Home'
+      activeColor='#e91e63'
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='home' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Messages'
+        component={Messages}
+        options={{
+          tabBarLabel: 'Messages',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='message' color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
+
+// export default function HomeStack() {
+//   return (
+//     <Stack.Navigator headerMode='none'>
+//       <Stack.Screen name='Home' component={HomeScreen} />
+//     </Stack.Navigator>
+//   );
+// }
