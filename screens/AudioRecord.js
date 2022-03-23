4826da34df7aa493c2f6eb332cf9d60b1ba8f40d
Lei Shi <leishi567@gmail.com>
2022-03-23 15:14:18 -0400

    audio clips can be saved to storage,but I realized that it should be saved to cloudinary first...

--- screens/AudioRecord.js
+++ screens/AudioRecord.js
@@ -2,7 +2,7 @@
 import React, {useState} from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { Audio } from 'expo-av';
-
+import firebase from 'firebase/app';
 export default function AudioRecord() {
   const [recording, setRecording] = useState();
   const [recordings, setRecordings] = useState([]);
@@ -23,6 +23,7 @@ export default function AudioRecord() {
         );
 
         setRecording(recording);
+        //console.log('recording when start',recording)
       } else {
         setMessage("Please grant permission to app to access microphone");
       }
@@ -31,19 +32,45 @@ export default function AudioRecord() {
     }
   }
 
+  // async function stopRecording() {
+  //   console.log('Stopping recording..');
+  //   setRecording(undefined);
+  //   await recording.stopAndUnloadAsync();
+  //   const uri = recording.getURI(); 
+  //   console.log('Recording stopped and stored at', uri);
+  //   const { sound: _sound, status } = await recording.createNewLoadedSoundAsync(
+  //     {
+  //       isLooping: true,
+  //       isMuted: false,
+  //       volume: 1.0,
+  //       rate: 1.0,
+  //       shouldCorrectPitch: true,
+  //     }
+  //   );
+
+  //   console.log('recording when stop',recording)
+  //   setRecording(recording)
+  // }
   async function stopRecording() {
     setRecording(undefined);
     await recording.stopAndUnloadAsync();
-
+      
     let updatedRecordings = [...recordings];
     const { sound, status } = await recording.createNewLoadedSoundAsync();
+    recording.sound = sound;
+    recording.duration = getDurationFormatted(status.durationMillis);
+
     updatedRecordings.push({
       sound: sound,
       duration: getDurationFormatted(status.durationMillis),
       file: recording.getURI()
     });
-
+    console.log('updatedRecordings when stop', updatedRecordings)
+  
     setRecordings(updatedRecordings);
+    setRecording(recording)
+    console.log('recording when stop', recording.sound, recording.duration)
+  
   }
 
   function getDurationFormatted(millis) {
@@ -54,15 +81,58 @@ export default function AudioRecord() {
     return `${minutesDisplay}:${secondsDisplay}`;
   }
 
+  const uploadAudio = async () => {
+    console.log('recording when save',recording)
+    const uri = recording.getURI();
+    try {
+      const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+          try {
+            resolve(xhr.response);
+          } catch (error) {
+            console.log("error:", error);
+          }
+        };
+        xhr.onerror = (e) => {
+          console.log(e);
+          reject(new TypeError("Network request failed"));
+        };
+        xhr.responseType = "blob";
+        xhr.open("GET", uri, true);
+        xhr.send(null);
+      });
+      if (blob != null) {
+        const uriParts = uri.split(".");
+        const fileType = uriParts[uriParts.length - 1];
+        firebase
+          .storage()
+          .ref()
+          .child(`nameOfTheFile.${fileType}`)
+          .put(blob, {
+            contentType: `audio/${fileType}`,
+          })
+          .then(() => {
+            console.log("Sent!");
+          })
+          .catch((e) => console.log("error:", e));
+      } else {
+        console.log("erroor with blob");
+      }
+    } catch (error) {
+      console.log("error:", error);
+    }
+  };
   function getRecordingLines() {
     return recordings.map((recordingLine, index) => {
       return (
         <View key={index} style={styles.row}>
-          <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+          <Text style={styles.fill}>Recording {index-1}- {recordingLine.duration}</Text>
           <Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+          <Button style={styles.button} onPress={uploadAudio} title="Save"></Button>
         </View>
       );
-    });
+     });
   }
 
   return (
@@ -75,7 +145,7 @@ export default function AudioRecord() {
       <StatusBar style="auto" />
     </View>
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
