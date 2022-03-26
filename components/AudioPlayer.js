182bf94683361486cbebf2e3c1c0424e3ff876f9
Lei Shi <leishi567@gmail.com>
2022-03-25 22:25:53 -0400

    need to fix audio player

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Audio } from "expo-av";
 //import { getAudio } from "../api/getAudio";
@@ -27,22 +27,18 @@ export default function AudioPlayer() {
         if (documentSnapshot.exists) {
           console.log("Audio Data", documentSnapshot.data().audio);
           const url = documentSnapshot.data().audio;
-          console.log("url", url);
           setAudioURL(url);
           console.log("url in getAudio", audioURL);
         }
       });
   };
-
-  //   console.log("audioURL", audioURL);
-
   //   useEffect(() => {
   //     if (playbackObject === null) {
   //       setPlaybackObject(sound);
   //     }
   //   }, []);
 
-  const handleAudioPlayPause = async () => {
+  const handleAudioPlayPause = async (url) => {
     getAudio();
     console.log("url in handleAudioPlay", audioURL);
     try {
@@ -50,11 +46,13 @@ export default function AudioPlayer() {
         await sound.pauseAsync();
         setIsPlaying(false);
       } else {
+       
+        setIsPlaying(true);
         await sound.loadAsync({
-          uri: audioURL
+          uri: url
         });
         await sound.playAsync();
-        setIsPlaying(true);
+        
       }
     } catch (error) {
       console.log(error);
@@ -83,22 +81,45 @@ export default function AudioPlayer() {
   };
 
   return (
-    <View style={{ flex: 1, padding: 50, backgroundColor: "#fff" }}>
+    <View style={styles.container}>
       <Text style={{ fontWeight: "bold", fontSize: 18, marginBottom: 15 }}>
         My story
       </Text>
-      <Ionicons
-        style={{
-          alignSelf: "center",
-          backgroundColor: "gray",
-          padding: 10,
-          borderRadius: 50,
-        }}
-        name={isPlaying ? "pause" : "play"}
-        size={24}
+      <TouchableOpacity 
+      style = {styles.control}
+      size={24}
         color="white"
-        onPress={handleAudioPlayPause}
-      />
+        onPress={()=>handleAudioPlayPause(audioURL)}>
+        {/* style={{
+        //   alignSelf: "center",
+        //   backgroundColor: "gray",
+        //   padding: 10,
+        //   borderRadius: 50,
+        // }} */}
+        {isPlaying ? (
+          <Ionicons name='ios-pause' size={48} color='#444' />
+        ) : (
+          <Ionicons name='ios-play-circle' size={48} color='#444' />
+        )}
+       
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  trackInfoText: {
+    textAlign: 'center',
+    flexWrap: 'wrap',
+    color: '#550088'
+  },
+  control: {
+    margin: 20
+  },
+})
