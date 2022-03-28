084b379272b08e7b0cd48cceb988f695f3c97c5f
Sara Sauceda <sarafsauceda@gmail.com>
2022-03-28 09:34:35 -0700

    working on user card on home screen

--- components/UserCard.js
+++ components/UserCard.js
@@ -8,9 +8,9 @@ const UserCard = () => {
         <SafeAreaView>
 <Card>
     <Card.Thumbnail
-        source={{ uri: 'https://picsum.photos/200' }}
-        style={{ height: 200, width: 300 }}
-        align={'center'}
+        source={{ uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
+        style={{ height: 85, width: 97 }}
+        align={'left'}
         // stretch
         // imageProps={{resizeMode: 'contain'}}
         containerStyle={{ justifyContent: 'center', alignItems: 'center' }}
@@ -21,10 +21,56 @@ const UserCard = () => {
         />
         <Card.Row>
         <Card.Col>
-            <Button mode={'outlined'}>demo</Button>
+            <Button mode={'outlined'}>Listen to Mary's Story</Button>
         </Card.Col>
         <Card.Col>
-            <Button mode={'contained'}>demo</Button>
+            <Button mode={'contained'}>Connect with Mary</Button>
+        </Card.Col>
+        </Card.Row>
+    </Card.Content>
+</Card>
+<Card>
+    <Card.Thumbnail
+        source={{ uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
+        style={{ height: 85, width: 97 }}
+        align={'left'}
+        // stretch
+        // imageProps={{resizeMode: 'contain'}}
+        containerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+    />
+    <Card.Content>
+        <Card.Title
+        text={'Ex Lorem magna sint labore ex commodo dolor minim ad.'}
+        />
+        <Card.Row>
+        <Card.Col>
+            <Button mode={'outlined'}>Listen to Mary's Story</Button>
+        </Card.Col>
+        <Card.Col>
+            <Button mode={'contained'}>Connect with Mary</Button>
+        </Card.Col>
+        </Card.Row>
+    </Card.Content>
+</Card>
+<Card>
+    <Card.Thumbnail
+        source={{ uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
+        style={{ height: 85, width: 97 }}
+        align={'left'}
+        // stretch
+        // imageProps={{resizeMode: 'contain'}}
+        containerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+    />
+    <Card.Content>
+        <Card.Title
+        text={'Ex Lorem magna sint labore ex commodo dolor minim ad.'}
+        />
+        <Card.Row>
+        <Card.Col>
+            <Button mode={'outlined'}>Listen to Mary's Story</Button>
+        </Card.Col>
+        <Card.Col>
+            <Button mode={'contained'}>Connect with Mary</Button>
         </Card.Col>
         </Card.Row>
     </Card.Content>
