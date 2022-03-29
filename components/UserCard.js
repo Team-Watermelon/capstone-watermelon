ffa4ad362fbe61a8358a78ee2b107562df7dc891
Sara Sauceda <sarafsauceda@gmail.com>
2022-03-28 22:23:28 -0700

    getting console log for user on usercard component

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,32 +1,59 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import Card from "react-native-card-component";
 import { SafeAreaView, StyleSheet} from 'react-native';
 import { Button } from 'react-native-paper';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
 
 const UserCard = () => {
+    const {user, logout} = useContext(AuthenticatedUserContext);
+    const [userData, setUserData] = useState(null);
+
+    const getUser = async () => {
+        await firebase
+          .firestore()
+          .collection("users")
+          .doc(user.uid)
+          .get()
+          .then((documentSnapshot) => {
+            if (documentSnapshot.exists) {
+                console.log('User Data', documentSnapshot.data());
+                setUserData(documentSnapshot.data());
+              }
+            });
+        };
+
+        useEffect(() => {
+            getUser();
+          }, []);
+
     return (
 <SafeAreaView >
     <Card>
-        <Card.Thumbnail
+        {/* <Card.Thumbnail
         source={{ uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
         style={{ height: 85, width: 97 }}
         align={'left'}
         // stretch
         // imageProps={{resizeMode: 'contain'}}
         containerStyle={{ justifyContent: 'center', alignItems: 'center' }}
-    />
+    /> */}
     <Card.Content>
         <Card.Title
-        text={'Ex Lorem magna sint labore ex commodo dolor minim ad.'}
-        />
-        <Card.Row>
+            color='black'>
+        {userData ? userData.city || 'Test' : 'Test'}{' '}
+        {/* text={'Ex Lorem magna sint labore ex commodo dolor minim ad.'}
+        /> */}
+        </Card.Title>
+        {/* <Card.Row>
         <Card.Col>
             <Button mode={'outlined'}>Listen to Mary's Story</Button>
         </Card.Col>
         <Card.Col>
             <Button mode={'contained'}>Connect with Mary</Button>
         </Card.Col>
-        </Card.Row>
+        </Card.Row> */}
     </Card.Content>
 </Card>
 <Card>
