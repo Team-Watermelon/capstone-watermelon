3a6dc5a0321def7d2012f255acf601cb23b4c920
Sara Sauceda <sarafsauceda@gmail.com>
2022-04-01 13:02:01 -0700

    icons lined up right on user card

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,12 +1,12 @@
 import React, {useContext, useEffect, useState} from 'react'
 import Card from "react-native-card-component";
-import { SafeAreaView, StyleSheet, View} from 'react-native';
+import { SafeAreaView, StyleSheet, View, Image} from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
-import NewAudioPlayer from './NewAudioPlayer';
+import NewestAudioPlayer from './NewestAudioPlayer';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const UserCard = ({item, onPress}) => {
@@ -31,86 +31,199 @@ const UserCard = ({item, onPress}) => {
             getUser();
           }, []);
 
-    return (
-<SafeAreaView >
-<TouchableOpacity onPress={onPress}>
-    <Card key={item.id}>
-        <Card.Thumbnail
-        source={{
-            uri: item
-              ? item.userImage ||
-                'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg'
-              : 'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg',
-          }} 
-            // uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
-        style={{ height: 85, width: 97 }}
-        align={'left'}
-        // stretch
-        // imageProps={{resizeMode: 'contain'}}
-        containerStyle={{ justifyContent: 'center', alignItems: 'center' }}
-    />
-        {/* <Card.Title> */}
-        <View style={{marginLeft: 16}}>
+          return (
+            <SafeAreaView>
+              <View
+                key={item.id}
+                style={{
+                  borderWidth: 2,
+                  borderColor: "#CDCDCD",
+                  margin: 10,
+                  borderRadius: 10,
+                //   shadowOpacity: 0.30,
+                //     shadowRadius: 4.65,
+                //     elevation: 8,
+                }}
+              >
+                <View style={{ flexDirection: "row",
+                // shadowOpacity: 0.30,
+                // shadowRadius: 4.65,
+                // elevation: 8,
+            }}        
+                >
+                    <TouchableOpacity onPress={onPress}>
+                  <Image
+                    source={{
+                      uri: item
+                        ? item.userImage ||
+                          "https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg"
+                        : "https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg",
+                    }}
+                    // uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
+                    style={{
+                      height: 97,
+                      width: 97,
+                      borderBottomLeftRadius: 10,
+                      borderTopLeftRadius: 10,
+                      // borderRadius: 25
+                    }}
+                    align={"left"}
+                    stretch
+                    imageProps={{ resizeMode: "contain" }}
+                    containerStyle={{ justifyContent: "center", alignItems: "center" }}
+                  />
+                  </TouchableOpacity>
+                  {/* <Card.Title> */}
+                  <View style={{ marginLeft: 16 }}>
+                    <TouchableOpacity onPress={onPress}>
+                      <Text
+                        style={{
+                          fontSize: 26,
+                          color: "#AC9292",
+                          fontWeight: "bold",
+                          fontFamily: "Arial",
+                          textTransform: "capitalize",
+                          paddingLeft: 15,
+                          paddingTop: 0,
+                        }}
+                      >
+                        {item ? item.firstName || "Test" : "Test"}{" "}
+                      </Text>
+                    </TouchableOpacity>
+                    {/* <Text
+                      style={{
+                        color: "#AC9292",
+                        fontSize: 16,
+                        marginLeft: 16
+                      }}
+                    >
+                      Listen to {item ? item.firstName || "Test" : "Test"} 's Story
+                    </Text> */}
+                    {/* <Button mode={'outlined'}>Listen to their story</Button> */}
+                    <NewestAudioPlayer url={item.audio} />
+                    <View
+                      style={{
+                        marginTop: 4,
+                        borderWidth: 0,
+                        paddingLeft: 15,
+                        flexDirection: "row",
+                        marginLeft: 8
+                        // width: '85%',
+                      }}
+                    >
+                      <Icon name="message" color="#AC9292" size={20}  />
+                      <Text
+                        style={{
+                          color: "#AC9292",
+                          fontSize: 16,
+                        }}
+                      >
+                        Connect with {item ? item.firstName || "Test" : "Test"}{" "}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </SafeAreaView>
+          );
+        };
+        
+        export default UserCard;
+        
+        const styles = StyleSheet.create({
+          container: {
+            flexDirection: "row",
+            alignItems: "center",
+            // justifyContent: 'center',
+            backgroundColor: "#f7f7f7",
+          },
+          box: {
+            width: 60,
+            height: 60,
+            marginVertical: 20,
+          },
+        });
+
+//     return (
+// <SafeAreaView >
+// <TouchableOpacity onPress={onPress}>
+//     <Card key={item.id}>
+//         <Card.Thumbnail
+//         source={{
+//             uri: item
+//               ? item.userImage ||
+//                 'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg'
+//               : 'https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg',
+//           }} 
+//             // uri: 'https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg' }}
+//         style={{ height: 85, width: 97 }}
+//         align={'left'}
+//         // stretch
+//         // imageProps={{resizeMode: 'contain'}}
+//         containerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+//     />
+//         {/* <Card.Title> */}
+//         <View style={{marginLeft: 16}}>
 
 
-            <Text
-              style={{
-                fontSize: 18,
-                color: '#AC9292',
-                fontWeight: 'bold',
-                fontFamily: 'Arial',
-                textTransform: 'capitalize',
-                paddingLeft: 15,
-                paddingTop: 0
-              }}>
-          {item ? item.firstName || 'Test' : 'Test'}{' '}
-          </Text>
+//             <Text
+//               style={{
+//                 fontSize: 18,
+//                 color: '#AC9292',
+//                 fontWeight: 'bold',
+//                 fontFamily: 'Arial',
+//                 textTransform: 'capitalize',
+//                 paddingLeft: 15,
+//                 paddingTop: 0
+//               }}>
+//           {item ? item.firstName || 'Test' : 'Test'}{' '}
+//           </Text>
          
-          <Text
-                style={{
-                  color: '#AC9292',
-                  fontSize: 14,
-                }}>
-                Listen to {item ? item.firstName || 'Test' : 'Test'}{' '}'s Story
-              </Text>     
-            {/* <Button mode={'outlined'}>Listen to their story</Button> */}
-            <NewAudioPlayer url ={item.audio} />
-            <View
-              style={{
-                marginTop: 4,
-                borderWidth: 0,
-                paddingLeft: 15,
-                flexDirection: 'row'
-                // width: '85%',
-              }}>
-               <Icon name="message" color='#AC9292' size={20} /> 
-              <Text
-                style={{
-                  color: '#AC9292',
-                  fontSize: 14,
-                }}>
-                Connect with {item ? item.firstName || 'Test' : 'Test'}{' '}
-              </Text>              
-            </View>
-            </View>
-</Card>
-</TouchableOpacity>
-</SafeAreaView>
-    )
-}
+//           <Text
+//                 style={{
+//                   color: '#AC9292',
+//                   fontSize: 14,
+//                 }}>
+//                 Listen to {item ? item.firstName || 'Test' : 'Test'}{' '}'s Story
+//               </Text>     
+//             {/* <Button mode={'outlined'}>Listen to their story</Button> */}
+//             <NewAudioPlayer url ={item.audio} />
+//             <View
+//               style={{
+//                 marginTop: 4,
+//                 borderWidth: 0,
+//                 paddingLeft: 15,
+//                 flexDirection: 'row'
+//                 // width: '85%',
+//               }}>
+//                <Icon name="message" color='#AC9292' size={20} /> 
+//               <Text
+//                 style={{
+//                   color: '#AC9292',
+//                   fontSize: 14,
+//                 }}>
+//                 Connect with {item ? item.firstName || 'Test' : 'Test'}{' '}
+//               </Text>              
+//             </View>
+//             </View>
+// </Card>
+// </TouchableOpacity>
+// </SafeAreaView>
+//     )
+// }
 
-export default UserCard;
+// export default UserCard;
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      // justifyContent: 'center',
-      backgroundColor: '#f7f7f7',
-    },
-    box: {
-      width: 60,
-      height: 60,
-      marginVertical: 20,
-    },
-  });
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       alignItems: 'center',
+//       // justifyContent: 'center',
+//       backgroundColor: '#f7f7f7',
+//     },
+//     box: {
+//       width: 60,
+//       height: 60,
+//       marginVertical: 20,
+//     },
+//   });
