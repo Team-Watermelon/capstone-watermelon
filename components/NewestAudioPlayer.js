67ef090039596ca7f43942f82ab3da354efbedc2
Jessica Donig <jessdonig@gmail.com>
2022-04-03 15:22:55 -0700

    fixed shadow

--- components/NewestAudioPlayer.js
+++ components/NewestAudioPlayer.js
@@ -59,9 +59,10 @@ export default function NewestAudio({ url }) {
 
 const styles = StyleSheet.create({
   playButtonContainer: {
+    elevation: 10,
     backgroundColor: "#FFF",
     borderColor: "rgba(93, 63, 106, 0.2)",
-    borderWidth: 15,
+    borderWidth: 1,
     width: 70,
     height: 70,
     borderRadius: 50,
@@ -70,11 +71,11 @@ const styles = StyleSheet.create({
     marginHorizontal: 32,
     shadowColor: "#333",
 shadowOffset: {
-	width: 0,
-	height: 2,
+	width: 3,
+	height: 3,
 },
 shadowOpacity: 0.25,
-shadowRadius: 3.84,
+shadowRadius: 1,
 
 elevation: 5
     // shadowColor: "#5D3F6A",
